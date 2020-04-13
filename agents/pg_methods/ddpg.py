b24407e9e8c5ec7c908095af1bf05f31f514f701
jason.harris <harris.e.jason@gmail.com>
2020-04-13 19:47:11 +0200

    again fix for cloud

--- agents/pg_methods/ddpg.py
+++ agents/pg_methods/ddpg.py
@@ -3,10 +3,12 @@ import torch.optim as optim
 import torch.autograd as autograd
 import torch.nn.functional as F
 import torch.nn as nn
-
-from common.replay_buffers import BasicBuffer
-from common.noise import OUNoise
+import numpy as np
+import math
+import random
+from collections import deque
 import os
+
 
 class DDPGAgent:
     
@@ -156,3 +158,181 @@ class Actor(nn.Module):
         x = torch.tanh(self.linear3(x))
 
         return x
+
+class BasicBuffer:
+
+    def __init__(self, max_size):
+        self.max_size = max_size
+        self.buffer = deque(maxlen=max_size)
+
+    def push(self, state, action, reward, next_state, done):
+        experience = (state, action, np.array([reward]), next_state, done)
+        self.buffer.append(experience)
+
+    def sample(self, batch_size):
+        state_batch = []
+        action_batch = []
+        reward_batch = []
+        next_state_batch = []
+        done_batch = []
+
+        batch = random.sample(self.buffer, batch_size)
+
+        for experience in batch:
+            state, action, reward, next_state, done = experience
+            state_batch.append(state)
+            action_batch.append(action)
+            reward_batch.append(reward)
+            next_state_batch.append(next_state)
+            done_batch.append(done)
+
+        return (state_batch, action_batch, reward_batch, next_state_batch, done_batch)
+
+    def sample_sequence(self, batch_size):
+        state_batch = []
+        action_batch = []
+        reward_batch = []
+        next_state_batch = []
+        done_batch = []
+
+        min_start = len(self.buffer) - batch_size
+        start = np.random.randint(0, min_start)
+
+        for sample in range(start, start + batch_size):
+            state, action, reward, next_state, done = self.buffer[start]
+            state, action, reward, next_state, done = experience
+            state_batch.append(state)
+            action_batch.append(action)
+            reward_batch.append(reward)
+            next_state_batch.append(next_state)
+            done_batch.append(done)
+
+        return (state_batch, action_batch, reward_batch, next_state_batch, done_batch)
+
+    def __len__(self):
+        return len(self.buffer)
+
+
+class NoisyLinear(nn.Module):
+
+    def __init__(self, num_in, num_out, is_training=True):
+        super(NoisyLinear, self).__init__()
+        self.num_in = num_in
+        self.num_out = num_out
+        self.is_training = is_training
+
+        self.mu_weight = nn.Parameter(torch.FloatTensor(num_out, num_in))
+        self.mu_bias = nn.Parameter(torch.FloatTensor(num_out))
+        self.sigma_weight = nn.Parameter(torch.FloatTensor(num_out, num_in))
+        self.sigma_bias = nn.Parameter(torch.FloatTensor(num_out))
+        self.register_buffer("epsilon_weight", torch.FloatTensor(num_out, num_in))
+        self.register_buffer("epsilon_bias", torch.FloatTensor(num_out))
+
+        self.reset_parameters()
+        self.reset_noise()
+
+    def forward(self, x):
+        self.reset_noise()
+
+        if self.is_training:
+            weight = self.mu_weight + self.sigma_weight.mul(autograd.Variable(self.epsilon_weight))
+            bias = self.mu_bias + self.sigma_bias.mul(autograd.Variable(self.epsilon_bias))
+        else:
+            weight = self.mu_weight
+            buas = self.mu_bias
+
+        y = F.linear(x, weight, bias)
+
+        return y
+
+    def reset_parameters(self):
+        std = math.sqrt(3 / self.num_in)
+        self.mu_weight.data.uniform_(-std, std)
+        self.mu_bias.data.uniform_(-std, std)
+
+        self.sigma_weight.data.fill_(0.017)
+        self.sigma_bias.data.fill_(0.017)
+
+    def reset_noise(self):
+        self.epsilon_weight.data.normal_()
+        self.epsilon_bias.data.normal_()
+
+
+class FactorizedNoisyLinear(nn.Module):
+
+    def __init__(self, num_in, num_out, is_training=True):
+        super(FactorizedNoisyLinear, self).__init__()
+        self.num_in = num_in
+        self.num_out = num_out
+        self.is_training = is_training
+
+        self.mu_weight = nn.Parameter(torch.FloatTensor(num_out, num_in))
+        self.mu_bias = nn.Parameter(torch.FloatTensor(num_out))
+        self.sigma_weight = nn.Parameter(torch.FloatTensor(num_out, num_in))
+        self.sigma_bias = nn.Parameter(torch.FloatTensor(num_out))
+        self.register_buffer("epsilon_i", torch.FloatTensor(num_in))
+        self.register_buffer("epsilon_j", torch.FloatTensor(num_out))
+
+        self.reset_parameters()
+        self.reset_noise()
+
+    def forward(self, x):
+        self.reset_noise()
+
+        if self.is_training:
+            epsilon_weight = self.epsilon_j.ger(self.epsilon_i)
+            epsilon_bias = self.epsilon_j
+            weight = self.mu_weight + self.sigma_weight.mul(autograd.Variable(epsilon_weight))
+            bias = self.mu_bias + self.sigma_bias.mul(autograd.Variable(epsilon_bias))
+        else:
+            weight = self.mu_weight
+            bias = self.mu_bias
+
+        y = F.linear(x, weight, bias)
+
+        return y
+
+    def reset_parameters(self):
+        std = 1 / math.sqrt(self.num_in)
+        self.mu_weight.data.uniform_(-std, std)
+        self.mu_bias.data.uniform_(-std, std)
+
+        self.sigma_weight.data.fill_(0.5 / math.sqrt(self.num_in))
+        self.sigma_bias.data.fill_(0.5 / math.sqrt(self.num_in))
+
+    def reset_noise(self):
+        eps_i = torch.randn(self.num_in)
+        eps_j = torch.randn(self.num_out)
+        self.epsilon_i = eps_i.sign() * (eps_i.abs()).sqrt()
+        self.epsilon_j = eps_j.sign() * (eps_j.abs()).sqrt()
+
+
+# Ornstein-Ulhenbeck Noise
+# Taken from #https://github.com/vitchyr/rlkit/blob/master/rlkit/exploration_strategies/ou_strategy.py
+class OUNoise(object):
+    def __init__(self, action_space, mu=0.0, theta=0.15, max_sigma=0.3, min_sigma=0.3, decay_period=100000):
+        self.mu = mu
+        self.theta = theta
+        self.sigma = max_sigma
+        self.max_sigma = max_sigma
+        self.min_sigma = min_sigma
+        self.decay_period = decay_period
+        self.action_dim = action_space.shape[0]
+        self.low = action_space.low
+        self.high = action_space.high
+        self.reset()
+
+    def reset(self):
+        self.state = np.ones(self.action_dim) * self.mu
+
+    def evolve_state(self):
+        x = self.state
+        dx = self.theta * (self.mu - x) + self.sigma * np.random.randn(self.action_dim)
+        self.state = x + dx
+        return self.state
+
+    def get_action(self, action, t=0):
+        ou_state = self.evolve_state()
+        self.sigma = self.max_sigma - (self.max_sigma - self.min_sigma) * min(1.0, t / self.decay_period)
+        return np.clip(action + ou_state, self.low, self.high)
+
--- agents/pg_methods/sac2019.py
+++ agents/pg_methods/sac2019.py
@@ -3,9 +3,12 @@ import torch.nn.functional as F
 import torch.optim as optim
 import torch.nn as nn
 from torch.distributions import Normal
+import random
+import numpy as np
+from collections import deque
 
 #from models import SoftQNetwork, PolicyNetwork
-from common.replay_buffers import BasicBuffer
+#from common.replay_buffers import BasicBuffer
 import os
 
 class SACAgent:
@@ -244,3 +247,57 @@ class PolicyNetwork(nn.Module):
         log_pi = log_pi.sum(1, keepdim=True)
 
         return action, log_pi
+
+
+class BasicBuffer:
+
+    def __init__(self, max_size):
+        self.max_size = max_size
+        self.buffer = deque(maxlen=max_size)
+
+    def push(self, state, action, reward, next_state, done):
+        experience = (state, action, np.array([reward]), next_state, done)
+        self.buffer.append(experience)
+
+    def sample(self, batch_size):
+        state_batch = []
+        action_batch = []
+        reward_batch = []
+        next_state_batch = []
+        done_batch = []
+
+        batch = random.sample(self.buffer, batch_size)
+
+        for experience in batch:
+            state, action, reward, next_state, done = experience
+            state_batch.append(state)
+            action_batch.append(action)
+            reward_batch.append(reward)
+            next_state_batch.append(next_state)
+            done_batch.append(done)
+
+        return (state_batch, action_batch, reward_batch, next_state_batch, done_batch)
+
+    def sample_sequence(self, batch_size):
+        state_batch = []
+        action_batch = []
+        reward_batch = []
+        next_state_batch = []
+        done_batch = []
+
+        min_start = len(self.buffer) - batch_size
+        start = np.random.randint(0, min_start)
+
+        for experience in range(start, start + batch_size):
+            state, action, reward, next_state, done = self.buffer[start]
+            state, action, reward, next_state, done = experience
+            state_batch.append(state)
+            action_batch.append(action)
+            reward_batch.append(reward)
+            next_state_batch.append(next_state)
+            done_batch.append(done)
+
+        return (state_batch, action_batch, reward_batch, next_state_batch, done_batch)
+
+    def __len__(self):
+        return len(self.buffer)
